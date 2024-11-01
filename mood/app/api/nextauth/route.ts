5ffe406ad93eff940367f841bef6f69d2fcacc4f
Leafd <leafd@marcusk.fun>
2024-10-31 19:28:35 -0600

    feat: add auth route

---
+++ mood/app/api/nextauth/route.ts
@@ -0,0 +1,28 @@
+import NextAuth from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+
+const scope = 'playlist-modify-public playlist-modify-private user-read-email';
+
+const handler = NextAuth({
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID!,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+            authorization: { params: { scope } }
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({ session, token }: { session: any; token: any }) {
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
+});
+
+export { handler as GET, handler as POST };
